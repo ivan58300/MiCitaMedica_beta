94394bbef8932e1b1b0d5e26df3ffdb41960ba07
AngeelDL <93502294+AngeelDL@users.noreply.github.com>
2021-11-01 13:03:12 -0600

    Update UIMenuDoctor.java

--- src/ui/UIMenuDoctor.java
+++ src/ui/UIMenuDoctor.java
@@ -117,6 +117,30 @@ public class UIMenuDoctor {
                 //registro de fechas disponibles.
                 UIMenu.doctorRegistrado.agregarCitasDisponibles(fecha,hora);
                 checkCitaDisponibleDoctor(UIMenu.doctorRegistrado);
+                
+                File archivo = new File("./CitasDoctores");
+                
+                if(!archivo.exists() ) {
+            	try {
+            		archivo.createNewFile();
+            	}catch (IOException e) {
+            		e.printStackTrace();
+            	    }
+                }
+            
+                try {
+                    FileWriter fw = new FileWriter(archivo, true);
+                    PrintWriter pw = new PrintWriter(fw);
+
+                    if(!pw.checkError()) {
+                        pw.println("Doctor: "+UIMenu.doctorRegistrado.getNombre());
+                        pw.println("Fecha: "+fecha);
+                        pw.println("Hora: "+hora);
+                    }
+                    pw.close();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }else if (respuesta == 0){
                 mostrarMenuDoctor();
