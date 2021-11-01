b353d7f2cd46d357353df7e499f44d5246825bca
Estebanphz <estebanph209@gmail>
2021-10-31 22:49:12 -0600

    Add Administrador

--- src/modelo/Administrador.java
+++ src/modelo/Administrador.java
@@ -4,16 +4,40 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Administrador extends Usuario{
+
     ArrayList<Doctor> doctores = new ArrayList<>();
+    private Int Id;
     public Administrador(int id, String nombre, String email, String contrasena) {
         super(id, nombre, email, contrasena);
     }
-private static void agragarDoctor(){
+
+    @Override
+    public Int getId() {
+        return Id;
+    }
+
+    public void setId(Int id) {
+        Id = id;
+    }
+
+    private static void agragarDoctor(){
     System.out.println("Ingresar nombre del Doctor: ");
     Scanner sc = new Scanner(System.in);
     String nombre = sc.nextLine();
+    System.out.println("Ingresar el correo del Doctor: ");
+    String correo = sc.nextLine();
+    System.out.println("Ingresar la direccion del Doctor: ");
+    String direccion = sc.nextLine();
+    System.out.println("Ingresar numero del Doctor: ");
+    String numero = sc.nextLine();
+    System.out.println("Ingresar contraseña del Doctor: ");
+    String pass = sc.nextLine();
+    boolean estatus = true;
+    System.out.println("Ingresar especialidad del Doctor: ");
+    String especialidad = sc.nextLine();
+
     //Int numero = Integer.valueOf(sc.nextLine());
-    doctores.add(new Doctor( nombre, "ivan@email.com", "Santa Catarina, N.L.",
-            "10935355","ivan123",true,"Cardiología" ));
+    doctores.add(new Doctor( nombre, correo, direccion,
+            numero,pass,true, ));
 }
 }
