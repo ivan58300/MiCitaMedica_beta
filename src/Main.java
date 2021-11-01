32de2b2e14c801a7cc359427e5003120457881c0
juancarlosdiaz <juankid2003@gmail.com>
2021-11-01 14:56:01 -0600

    clase archivos

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.ConcurrentModificationException;
 import static ui.UIMenu.mostrarMenu;
 
 public class Main {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         try{
             mostrarMenu();
         }catch (ConcurrentModificationException e){
