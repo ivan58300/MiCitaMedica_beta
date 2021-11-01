7e9dc333572e72d48718e3454ddbee328a908b31
Estebanphz <estebanph209@gmail>
2021-11-01 00:45:28 -0600

    Add Administrador new

---
+++ src/ui/UIMenuAdministrador.java
@@ -0,0 +1,90 @@
+package ui;
+
+import modelo.Doctor;
+import modelo.Paciente;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UIMenuAdministrador {
+
+    public static void mostrarMenuAdministrador(){
+        int respuesta = 0;
+        do {
+
+            System.out.println("\n\n");
+            System.out.println("\t- Administrador -");
+            System.out.println("1. Agregar doctor");
+            System.out.println("2. Agregar paciente");
+            System.out.println("0. Regresar ");
+
+            Scanner sc = new Scanner(System.in);
+            respuesta = sc.nextInt();
+
+            switch (respuesta){
+                case 1:
+                    respuesta = 0;
+                    agregarDoctor();
+                    break;
+                case 2:
+                    respuesta = 0;
+                    agregarPaciente();
+                    break;
+                case 0:
+                    UIMenu.mostrarMenu();
+                    UIMenu.seccion = 1;
+                    break;
+
+            }
+
+        }while (respuesta != 0);
+    }
+
+    //Metodo
+    private static void agregarDoctor(){
+        ArrayList<Doctor> doctores = new ArrayList<>();
+        System.out.println("Ingresar nombre del Doctor: ");
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.nextLine();
+        System.out.println("Ingresar el correo del Doctor: ");
+        String correo = sc.nextLine();
+        System.out.println("Ingresar la direccion del Doctor: ");
+        String direccion = sc.nextLine();
+        System.out.println("Ingresar numero del Doctor: ");
+        String numero = sc.nextLine();
+        System.out.println("Ingresar contraseña del Doctor: ");
+        String pass = sc.nextLine();
+        boolean estatus = true;
+        System.out.println("Ingresar especialidad del Doctor: ");
+        String especialidad = sc.nextLine();
+        //Int numero = Integer.valueOf(sc.nextLine());
+        doctores.add(new Doctor( nombre, correo, direccion,
+                numero, pass,true, especialidad));
+    }
+
+    private static void agregarPaciente(){
+        ArrayList <Paciente> pacientes = new ArrayList<>();
+        float peso, altura;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresar nombre del paciente:");
+        String nombrep = sc.nextLine();
+        System.out.println("Ingresar correo del paciente:");
+        String correop = sc.nextLine();
+        System.out.println("Ingresar direccion del paciente:");
+        String direccionp = sc.nextLine();
+        System.out.println("Ingresar telefono del paciente:");
+        String telefonop = sc.nextLine();
+        System.out.println("Ingresar contrasena del paciente:");
+        String passp = sc.nextLine();
+        System.out.println("Ingresar fecha de nacimiento del paciente:");
+        String fecha = sc.nextLine();
+        System.out.println("Ingresar tipo de sangre del paciente:");
+        String sangre = sc.nextLine();
+        System.out.println("Ingresar peso del paciente:");
+        peso = sc.nextFloat();
+        System.out.println("Ingresar altura del paciente:");
+        altura = sc.nextFloat();
+        pacientes.add(new Paciente(nombrep, correop, direccionp, telefonop, passp, true , fecha, sangre, peso, altura ));
+
+    }
+}
